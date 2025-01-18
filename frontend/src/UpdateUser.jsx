13cb5761b96f1e5d97c16c8a1ebfaedbdd0f74ff
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-01-18 19:54:09 +0530

    delete

--- frontend/src/UpdateUser.jsx
+++ frontend/src/UpdateUser.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 function UpdateUser() {
@@ -8,16 +9,31 @@ function UpdateUser() {
   const [age, setAge] = useState();
   const navigate = useNavigate();
 
-  useEffect(() => { 
-    axios.get('http://localhost:3001/getUser'+id)
-    .then(result => console.log(result))
+  useEffect(() => {
+    axios.get("http://localhost:3001/getUser/" + id)
+    .then((result) => {
+      console.log(result);
+      setName(result.data.name);
+      setEmail(result.data.email);
+      setAge(result.data.age);
+    })
+    .catch(err => console.log(err));
+  }, []);
+
+  const Update = (e) => {
+    e.preventDefault();
+    axios.put("http://127.0.0.1:3001/updateUser" + id, { name, email, age })
+    .then((result) => {
+      console.log(result)
+      navigate("/")
+    })
     .catch(err => console.log(err))
-  }, [])
+  };
 
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <form>
+        <form onSubmit={Update}>
           <h2>Update User</h2>
           <div className="mb-2">
             <label htmlFor="">Name</label>
@@ -25,6 +41,8 @@ function UpdateUser() {
               type="text"
               placeholder="Enter Name"
               className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -33,6 +51,8 @@ function UpdateUser() {
               type="email"
               placeholder="Enter Email"
               className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -41,6 +61,8 @@ function UpdateUser() {
               type="number"
               placeholder="Enter Age"
               className="form-control"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
             />
           </div>
           <button className="btn btn-success">Update</button>
